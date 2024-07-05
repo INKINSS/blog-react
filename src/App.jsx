61b279aedca3eab5d048a2cf723c12ebbcdb9d0d
samito <martinsamuelpp@gmail.com>
2024-07-05 10:15:40 -0600

    implementacion de react-router-dom realizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import Blog from "./pages/Blog";
 import Initial from "./pages/Initial";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Initial />
-    </>
+    <Routes>
+      <Route path="/" element={<Initial />} />
+      <Route path="/blog" element={<Blog />} />
+    </Routes>
   );
 }
 
