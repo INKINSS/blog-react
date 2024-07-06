c9a9bcd43b98b1ab0190a2e71199efe2bfea7d4b
samito <martinsamuelpp@gmail.com>
2024-07-05 18:41:49 -0600

    cambios pequeños en barra de busqueda y menu mobile

--- src/common/MenuMobile.jsx
+++ src/common/MenuMobile.jsx
@@ -17,10 +17,10 @@ const MenuMobile = () => {
   ];
 
   return (
-    <header>
+    <header className="relative">
       <nav
-        className={`fixed top-0 right-0 w-full h-screen bg-white transform transition-transform duration-300 ease-in-out ${
-          menuVisible ? "translate-x-0" : "translate-x-full"
+        className={`fixed top-0 z-10 right-0 w-full h-full bg-white transform transition-transform duration-300 ease-in-out ${
+          menuVisible ? "translate-x-[20%]" : "translate-x-full"
         }`}
       >
         <ul className="pt-14">
@@ -34,11 +34,9 @@ const MenuMobile = () => {
       <div className="flex justify-end">
         <MdArrowBackIos
           size={"2rem"}
-          className={`m-5 flex cursor-pointer ${
-            menuVisible ? "rotate-180" : "rotate-0"
-          } ${
-            menuVisible ? "fixed" : "block"
-          } transform transition-transform duration-300 ease-in-out`}
+          className={`m-5 cursor-pointer z-20 transform transition-transform duration-300 ease-in-out text-grayLight ${
+            menuVisible ? "rotate-180 fixed" : "rotate-0 absolute"
+          }`}
           onClick={handleClick}
         />
       </div>
--- src/common/Search.jsx
+++ src/common/Search.jsx
@@ -2,9 +2,9 @@ import { IoSearch } from "react-icons/io5";
 
 const Search = () => {
   return (
-    <div className="relative flex justify-center my-8">
-      <div className="flex items-center">
-        <IoSearch className="absolute ml-2 text-grayLight" />
+    <div className="flex justify-center my-8">
+      <div className="flex items-center relative">
+        <IoSearch className="ml-4 text-grayLight absolute" />
       </div>
       <input
         className="border-2 border-grayLight text-grayLight text-[.8rem] text-center rounded-full py-2 px-6 focus:border-grayLight shadow-lg"
