7806cb637bcd40d6017995dad23acdeba7750791
samito <martinsamuelpp@gmail.com>
2024-07-12 16:37:14 -0600

    cambios en vistas de blog y barra de busqueda

--- src/common/Search.jsx
+++ src/common/Search.jsx
@@ -7,7 +7,7 @@ const Search = () => {
         <IoSearch className="ml-4 text-grayLight absolute" />
       </div>
       <input
-        className="border-2 border-grayLight text-grayLight text-[.8rem] text-center rounded-full py-2 px-6 focus:border-grayLight shadow-lg"
+        className="border-2 border-grayExtraLight h-[3rem] w-[16rem] text-grayLight text-[.9rem] text-center rounded-full py-2 px-10 focus:border-grayLight shadow-lg"
         type="text"
         placeholder="¿buscas algo?"
       />
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -30,35 +30,36 @@ const Blog = () => {
       <main className="mt-20">
         <Logo />
         <Search />
-        <article className="px-10">
+        <article className="px-10 pb-32">
           <h2 className="text-primary font-semibold text-[1.2rem]">
             Artículos recientes
           </h2>
-          <article className="w-full">
+          <article className="w-full flex gap-5">
             {loading ? (
               <p>Cargando...</p>
             ) : (
               data.map((item) => (
                 <article
-                  className="max-w-[13rem] min-h-[20rem] shadow-card rounded-lg py-6 px-5 mt-10"
+                  className="max-w-[23rem] shadow-card rounded-lg py-6 px-5 mt-10 flex flex-col justify-between"
                   key={item.id}
                   onClick={() => handleArticleClick(item.id)}
                 >
-                  <h3 className="font-bold text-[1.5rem]">{item.title}</h3>
-                  <p className="text-grayLight font-normal mt-1 mb-3">
-                    {item.description}
-                  </p>
+                  <div>
+                    <h3 className="font-bold text-[1.3rem] text-primary">{item.title}</h3>
+                    <p className="text-grayLight font-normal mt-1 mb-3">
+                      {item.description}
+                    </p>
+                  </div>
+
                   <section className="w-full flex gap-2 flex-wrap">
-                    {data.map((item) =>
-                      item.tags.map((tag, index) => (
-                        <span
-                          className="text-sm block border border-grayExtraLight rounded-full px-3"
-                          key={`${item.id}-${index}`}
-                        >
-                          {tag}
-                        </span>
-                      ))
-                    )}
+                    {item.tags.map((tag, index) => (
+                      <span
+                        className="text-[.85rem] font-light block border border-primary text-primary rounded-full px-3 py-1 hover:text-white hover:bg-primary transform duration-300"
+                        key={`${item.id}-${index}`}
+                      >
+                        {tag}
+                      </span>
+                    ))}
                   </section>
                 </article>
               ))
