4449e5e246cce92fe33f84079e4cf73cafa90741
samito <martinsamuelpp@gmail.com>
2024-07-13 17:59:14 -0600

    mejoramiento de vista en fetch de datos

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 import useFetchData from "../hooks/useFetchData";
 
 const Blog = () => {
-  const { data, error } = useFetchData("http://localhost:3000"); // Llamamos a useFetchData directamente
+  const { data, error } = useFetchData("http://localhost:3000");
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
@@ -27,44 +27,44 @@ const Blog = () => {
   return (
     <>
       <MenuMobile />
-      <main className="mt-20">
+      <main className="mt-20 w-full">
         <Logo />
         <Search />
-        <article className="px-10 pb-32">
-          <h2 className="text-primary font-semibold text-[1.2rem]">
-            Artículos recientes
-          </h2>
-          <article className="w-full flex gap-5">
-            {loading ? (
-              <p>Cargando...</p>
-            ) : (
-              data.map((item) => (
-                <article
-                  className="max-w-[23rem] shadow-card rounded-lg py-6 px-5 mt-10 flex flex-col justify-between"
-                  key={item.id}
-                  onClick={() => handleArticleClick(item.id)}
-                >
-                  <div>
-                    <h3 className="font-bold text-[1.3rem] text-primary">{item.title}</h3>
-                    <p className="text-grayLight font-normal mt-1 mb-3">
-                      {item.description}
-                    </p>
-                  </div>
+        <h2 className="text-primary px-10 font-semibold text-[1.2rem] flex justify-start">
+          Artículos recientes
+        </h2>
+        <article className="px-10 pb-32 flex justify-center flex-wrap">
+          {loading ? (
+            <p>Cargando...</p>
+          ) : (
+            data.map((item) => (
+              <article
+                className="shadow-card rounded-lg py-6 px-5 mt-10 w-[14rem] max-w-[15rem] md:w-1/3 lg:w-1/4 xl:w-1/5 mx-5 hover:shadow-hoverCard transform duration-300 cursor-pointer"
+                key={item.id}
+                onClick={() => handleArticleClick(item.id)}
+              >
+                <div>
+                  <h3 className="font-bold text-[1.3rem] text-primary">
+                    {item.title}
+                  </h3>
+                  <p className="text-grayLight font-normal mt-1 mb-3">
+                    {item.description}
+                  </p>
+                </div>
 
-                  <section className="w-full flex gap-2 flex-wrap">
-                    {item.tags.map((tag, index) => (
-                      <span
-                        className="text-[.85rem] font-light block border border-primary text-primary rounded-full px-3 py-1 hover:text-white hover:bg-primary transform duration-300"
-                        key={`${item.id}-${index}`}
-                      >
-                        {tag}
-                      </span>
-                    ))}
-                  </section>
-                </article>
-              ))
-            )}
-          </article>
+                <section className="w-full flex gap-2 flex-wrap">
+                  {item.tags.map((tag, index) => (
+                    <span
+                      className="text-[.85rem] font-light block border border-primary text-primary rounded-full px-3 py-1 hover:text-white hover:bg-primary transform duration-300"
+                      key={`${item.id}-${index}`}
+                    >
+                      {tag}
+                    </span>
+                  ))}
+                </section>
+              </article>
+            ))
+          )}
         </article>
       </main>
     </>
