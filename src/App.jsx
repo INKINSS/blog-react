8d51cb509270083b32de13635a9916c363da7485
samito <martinsamuelpp@gmail.com>
2024-07-20 16:57:11 -0600

    vista de colaborar añadida, dependencias de fecha desinstaladas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Blog from "./pages/Blog";
+import Collaborate from "./pages/Collaborate";
 import DetailArticle from "./pages/DetailArticle";
 import Initial from "./pages/Initial";
 import { Routes, Route } from "react-router-dom";
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Initial />} />
       <Route path="/blog" element={<Blog />} />
       <Route path="/article/:id" element={<DetailArticle />} />
+      <Route path="/collaborate" element={<Collaborate />} />
     </Routes>
   );
 }
