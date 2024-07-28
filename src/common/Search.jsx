4ef9724f5891ae4ee537b40b5f7621c581b56adf
samito <martinsamuelpp@gmail.com>
2024-07-28 13:17:30 -0600

    barra de busqueda por titulo y etiqueta funcional

--- src/common/Search.jsx
+++ src/common/Search.jsx
@@ -1,6 +1,7 @@
 import { IoSearch } from "react-icons/io5";
+import PropTypes from 'prop-types'
 
-const Search = () => {
+const Search = ({searchTerm, setSearchTerm}) => {
   return (
     <div className="flex justify-center my-8">
       <div className="flex items-center relative">
@@ -9,6 +10,8 @@ const Search = () => {
       <input
         className="border-2 border-grayExtraLight h-[3rem] w-[16rem] text-grayLight text-[.9rem] text-center rounded-full py-2 px-10 focus:border-grayLight shadow-lg"
         type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
         placeholder="¿buscas algo?"
       />
     </div>
@@ -16,3 +19,8 @@ const Search = () => {
 };
 
 export default Search;
+
+Search.propTypes = {
+  searchTerm: PropTypes.string.isRequired,
+  setSearchTerm: PropTypes.func.isRequired
+}
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -11,6 +11,7 @@ const Blog = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,6 +34,12 @@ const Blog = () => {
     navigate(`/article/${id}`);
   };
 
+  const filteredData = data.filter(item => {
+    const matchesTitle = item.detail.title.toLowerCase().includes(searchTerm.toLowerCase());
+    const matchesTag = item.detail.tags.some(tag => tag.toLowerCase().includes(searchTerm.toLowerCase()));
+    return matchesTitle || matchesTag;
+  });
+
   if (error) {
     return <div>Error: {error.message}</div>;
   }
@@ -42,7 +49,7 @@ const Blog = () => {
       <MenuMobile />
       <main className="mt-20 w-full">
         <Logo />
-        <Search />
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         <h2 className="text-primary px-10 font-semibold text-[1.2rem] flex justify-start">
           Artículos recientes
         </h2>
@@ -50,34 +57,40 @@ const Blog = () => {
           {loading ? (
             <p>Cargando...</p>
           ) : (
-            data.map((item) => (
-              <article
-                className="shadow-card rounded-lg py-6 px-5 mt-10 w-[14rem] max-w-[15rem] md:w-1/3 lg:w-1/4 xl:w-1/5 mx-5 hover:shadow-hoverCard transform duration-300 cursor-pointer"
-                key={item.id}
-                onClick={() => handleArticleClick(item.id)}
-              >
-                <div>
-                  <h3 className="font-bold text-[1.3rem] text-primary">
-                    {item.detail.title}
-                  </h3>
-                  <p className="text-grayLight font-normal mt-1 mb-3">
-                    {item.detail.description}
-                  </p>
-                </div>
+            filteredData.length > 0 ? (
+              filteredData.map((item) => (
+                <article
+                  className="shadow-card rounded-lg py-6 px-5 mt-10 w-[14rem] max-w-[15rem] md:w-1/3 lg:w-1/4 xl:w-1/5 mx-5 hover:shadow-hoverCard transform duration-300 cursor-pointer"
+                  key={item.id}
+                  onClick={() => handleArticleClick(item.id)}
+                >
+                  <div>
+                    <h3 className="font-bold text-[1.3rem] text-primary">
+                      {item.detail.title}
+                    </h3>
+                    <p className="text-grayLight font-normal mt-1 mb-3">
+                      {item.detail.description}
+                    </p>
+                  </div>
 
-                <section className="w-full flex gap-2 flex-wrap">
-                  {item.detail.tags &&
-                    item.detail.tags.map((tag, index) => (
-                      <span
-                        className="text-[.85rem] font-light block border border-primary text-primary rounded-full px-3 py-1 hover:text-white hover:bg-primary transform duration-300"
-                        key={`${item.id}-${index}`}
-                      >
-                        {tag}
-                      </span>
-                    ))}
-                </section>
-              </article>
-            ))
+                  <section className="w-full flex gap-2 flex-wrap">
+                    {item.detail.tags &&
+                      item.detail.tags.map((tag, index) => (
+                        <span
+                          className="text-[.85rem] font-light block border border-primary text-primary rounded-full px-3 py-1 hover:text-white hover:bg-primary transform duration-300"
+                          key={`${item.id}-${index}`}
+                        >
+                          {tag}
+                        </span>
+                      ))}
+                  </section>
+                </article>
+              ))
+            ) : (
+              <h3 className="text-primary font-semibold text-[1.2rem] mt-10">
+                No se encontraron artículos que coincidan con tu búsqueda.
+              </h3>
+            )
           )}
         </article>
         <div className="flex justify-center pb-10">
